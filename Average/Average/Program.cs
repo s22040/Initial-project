8b7daed7b2692ea88dde3be6f9392f61097bef2f
s22040 <s22040@pjwstk.edu.pl>
2024-03-24 22:57:52 +0100

    Changed Average variable name

--- Average/Average/Program.cs
+++ Average/Average/Program.cs
@@ -3,11 +3,11 @@
     public static void count_avg()
     {
         int[] numbers = {4, 6, 8, 10, 12};
-        int sum = 0;
+        int suma = 0;
 
         for (int i = 0; i < numbers.Length; i++)
         {
-            sum += numbers[i];
+            suma += numbers[i];
         }
 
         double average = (double)sum / numbers.Length;

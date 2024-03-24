d438a663c2c292d9d41c049c4cfba3f7b15012f3
s22040 <s22040@pjwstk.edu.pl>
2024-03-24 22:39:59 +0100

    feature-max

---
+++ Max/Max/Program.cs
@@ -0,0 +1,19 @@
+﻿class Max
+{
+    public static void find_max()
+    {
+        int[] numbers = {1, 3, 2, 5, 6, 7, 12, 34, 50, 99, 97, 96, 111, 110};
+
+        int max_num = numbers[0];
+        for (int index = 1; index < numbers.Length; index++)
+        {
+            if (numbers[index] > max_num)
+                max_num = numbers[index];
+        }
+        Console.WriteLine("Max: " + max_num);
+    }
+    static void Main()
+    {
+        Max.find_max();
+    }
+}
